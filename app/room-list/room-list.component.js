94cee84600b5e0495faaf7c97f23aad983773ecf
cjt <taylor.christopher@gmail.com>
2019-01-23 18:34:03 +0000

    Refactored to make room list a component. Wired up and it renders.

--- app/room-list/room-list.component.js
+++ app/room-list/room-list.component.js
@@ -5,6 +5,15 @@ angular
     .component('roomList', {
         templateUrl: 'room-list/room-list.template.html',
         controller: function roomListController() {
-            
+            this.rooms = [
+                {
+                    name: "Mountain Rescue",
+                    messages: 2
+                },
+                {
+                    name: "RNLI",
+                    messages: 1
+                }
+            ];
         }
     });
