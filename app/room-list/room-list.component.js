c75bcb4313b4b34827f6418c15091d402daa55ce
cjt <taylor.christopher@gmail.com>
2019-01-23 21:36:18 +0000

    Modified code to minify-proof per angjs tutorial.

--- app/room-list/room-list.component.js
+++ app/room-list/room-list.component.js
@@ -4,7 +4,7 @@ angular
     .module('roomList')
     .component('roomList', {
         templateUrl: 'room-list/room-list.template.html',
-        controller: function roomListController($http) {
+        controller: ['$http', function roomListController($http) {
             var self = this;
             $http.get('http://localhost:5984/chat1/_design/rooms/_view/rooms?group=true').then(function(response) {
                 var rows = response.data.rows;
@@ -13,8 +13,8 @@ angular
                 rows.forEach(function(row) {
                     rooms.push({ "name":row.key, "messages":row.value })
                 });
-                
+
                 self.rooms = rooms;
             });
-        }
+        }]
     });
