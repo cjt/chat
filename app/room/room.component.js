6894b4a6189e0589554d37d3b54715b51b5762f9
cjt <taylor.christopher@gmail.com>
2019-01-24 11:26:27 +0000

    Backfilled message retrieval, not properly routed yet.

--- app/room/room.component.js
+++ app/room/room.component.js
@@ -4,10 +4,19 @@ angular.
   module('room').
   component('room', {
     templateUrl: 'room/room.template.html',
-    controller: ['$http', '$routeParams',
-		 function RoomController($http, $routeParams) {
-		   var self = this;
-		   // get room details from couch...
-		   this.room = $routeParams.room;
-		 }]
+    controller: ['$http', '$routeParams', function RoomController($http, $routeParams) {
+      var self = this;
+      var room = $routeParams.room; // sanitize this...
+      
+      $http.get('http://127.0.0.1:5984/chat1/_design/messages/_view/messages?key=' + room + '&include_docs=true').then(function(response) {
+        var rows = response.data.rows;
+        var messages = [];
+
+        rows.forEach(function(row) {
+          messages.push({ "room":row.doc.room, "datetime":row.doc.datetime, "username":row.doc.user, "message":row.doc.message })
+        });
+
+        self.messages = messages;  
+      });
+    }]
   });
