c7cc06268ae1957d72f1a34eb570bbde1162741e
cjt <taylor.christopher@gmail.com>
2019-01-25 10:03:08 +0000

    Added interval based refresh of room list to update message counts.

--- app/room-list/room-list.component.js
+++ app/room-list/room-list.component.js
@@ -4,7 +4,7 @@ angular
   .module('roomList')
   .component('roomList', {
     templateUrl: 'room-list/room-list.template.html',
-    controller: ['$http', '$scope', function roomListController($http, $scope) {
+    controller: ['$http', '$scope', '$interval', function roomListController($http, $scope, $interval) {
       $scope.select = function(room) {
 	$scope.$parent.room = room;
 	$scope.$parent.loadMessages();
@@ -12,16 +12,23 @@ angular
       };
       
       var self = this;
-      $http.get('http://localhost:5984/chat/_design/rooms/_view/rooms?group=true').then(function(response) {
-        var rows = response.data.rows;
-        var rooms = [];
 
-        rows.forEach(function(row) {
-          rooms.push({ "name":row.key, "messages":row.value })
-        });
+      var loadRoomList = function() {      
+	$http.get('http://localhost:5984/chat/_design/rooms/_view/rooms?group=true').then(function(response) {
+          var rows = response.data.rows;
+          var rooms = [];
 
-        self.rooms = rooms;
-      });
+          rows.forEach(function(row) {
+            rooms.push({ "name":row.key, "messages":row.value })
+          });
+
+          self.rooms = rooms;
+	});
+      }
+
+      $interval(function() {
+	loadRoomList();
+      }, 500);
     }]
   });
 
